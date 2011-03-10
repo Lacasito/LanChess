9f17547c3fce245d98c1cc1ab9644d07b346b9ea
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2011-03-10 18:59:47 -0500

    GraphicsEngine first interface version done

--- GraphicsEngine.h
+++ GraphicsEngine.h
@@ -10,7 +10,29 @@
 
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+#include <string>
+#include "CommonTypes.h"
 
+
+class GraphicsEngine {
+
+	private:
+		int bpp, height, width;
+
+		SDL_Surface *screen;
+		SDL_Surface *pawnSrf, *rookSrf, *knightSrf, *bishopSrf, *kingSrf, *queenSrf;
+		SDL_Surface *board;
+
+		SDL_Surface *loadImg (std::string path);
+		void applySurface(SDL_Surface *source, SDL_Surface *destination);
+
+	public:
+		GraphicsEngine(int BPP, int WIDTH, int HEIGHT, std::string path);
+		~GraphicsEngine();
+		void init(bool& errorCode);
+		void drawBoard();
+		void drawPiece(LCVAR_PieceType type, int x, int y);
+};
 
 
 #endif /* GRAPHICSENGINE_H_ */
