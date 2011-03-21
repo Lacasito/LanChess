44f2ae055a9912992537b263fb55aadd344a6b76
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2011-03-21 19:44:55 -0400

    changed bpp to 32

--- Game.cpp
+++ Game.cpp
@@ -26,7 +26,7 @@ void Game::run() {
 	short errorCode = 0;
 
 	//TODO: Don't hardcode these settings
-	engine->init(16, 600, 600, ".", errorCode);
+	engine->init(32, 600, 600, ".", errorCode);
 
 	handleErrors(errorCode);
 
