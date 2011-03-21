e8ab4de879a3acd9743d8162594885be6b3c7c7c
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2011-03-20 23:20:23 -0400

    Created necessary types for event handling

--- CommonTypes.h
+++ CommonTypes.h
@@ -1,7 +1,17 @@
 #ifndef COMMONTYPES_H
 #define COMMONTYPES_H
 
+#include "string"
+
 enum LCVAR_PieceType { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN };
 enum LCVAR_Color { BLACK, WHITE };
+enum LCVAR_EventType { MOVE };
+
+typedef struct {
+
+	LCVAR_EventType type;
+	std::string meta;
+
+} LCVAR_Event;
 
 #endif 
