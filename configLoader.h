90aa18f84ae44c534dd89a74e24ed3b9efdb9609
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2011-03-30 11:31:29 +1100

    Added disclaimer info

--- configLoader.h
+++ configLoader.h
@@ -1,3 +1,20 @@
+/*  LanChess
+    Copyright (C) 2011  Eugenio Cano-Manuel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef CONFIG_LOADER_H
 #define CONFIG_LOADER_H
 
@@ -14,6 +31,7 @@ private:
 public:
 	configLoader(string fileName);
 	~configLoader();
+	void loadData();
 	void getPawnFile(string& filename);
 	void getRookFile(string& filename);
 	void getKnightFile(string& filename);
