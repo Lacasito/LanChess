a0e921f3c8b0e2c8fda3ff25b3c0600092023444
Eugenio Cano-Manuel <eugeniocanom@gmail.com>
2011-03-30 11:36:28 +1100

    Added implementation of the configLoader class

---
+++ configLoader.cpp
@@ -0,0 +1,37 @@
+#include "configLoader.h"
+
+configLoader::configLoader(string& fileName){
+	configFile = filename;
+}
+
+configLoader::~configLoader(){
+
+}
+
+void configLoader::loadData(){
+//TODO: Implement
+}
+
+void configLoader::getPawnFile(string& filename){
+	filename = pawnFile;
+}
+
+void configLoader::getRookFile(string& filename){
+	filename = rookFile;
+}
+
+void configLoader::getKnightFile(string& filename){
+	filename = knightFile;
+}
+
+void configLoader::getBishopFile(string& filename){
+	filename = bishopFile;
+}
+
+void configLoader::getKingFile(string& filename){
+	filename = kingFile;
+}
+
+void configLoader::getQueenFile(string& filename){
+	filename = queenFile;
+}
