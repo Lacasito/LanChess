bca59a725ee51374da2bf760a09533cde108fb36
keidesu <eugeniocanom@gmail.com>
2011-04-19 13:16:40 -0400

    Added FIXME

--- GraphicsEngine.cpp
+++ GraphicsEngine.cpp
@@ -15,6 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+//FIXME: Close sockets at the end of the program execution!
+
 #include "GraphicsEngine.h"
 #include "configLoader.h"
 #include <iostream>
